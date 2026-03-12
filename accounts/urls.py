6d07ada7079c583fd845805a5c2b6e787d9a5dcf
Alex Edwards <3017116e@student.gla.ac.uk>
2026-03-12 22:55:05 +0000

    added logo to navigation bar and basic home page layout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "accounts" 
+
 urlpatterns = [
     path("login/", views.auth_page, name="login"),
     path("logout/", views.logout_user, name="logout"),
